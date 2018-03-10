5eab88b9139803c2c704ed088f65e32044de4174
nabin01 <nabinkoirala01.nk@gmail.com>
2018-03-11 00:59:42 +0545

    initial commit

--- ttt.js
+++ ttt.js
@@ -1,71 +1,71 @@
 var N_SIZE = 3,
     EMPTY = "&nbsp;",
-	boxes = [],
-	turn = "X",
-	score,
-	moves;
+    boxes = [],
+    turn = "X",
+    score,
+    moves;
 
 // Initializes the Tic Tac Toe board and starts the game.
 function init() {
-	var board = document.createElement('table');
+    var board = document.createElement('table');
     board.setAttribute("border", 1);
     board.setAttribute("cellspacing", 0);
-    
-	var identifier = 1;
-	for (var i = 0; i < N_SIZE; i++) {
-		var row = document.createElement('tr');
-		board.appendChild(row);
-		for (var j = 0; j < N_SIZE; j++) {
+
+    var identifier = 1;
+    for (var i = 0; i < N_SIZE; i++) {
+        var row = document.createElement('tr');
+        board.appendChild(row);
+        for (var j = 0; j < N_SIZE; j++) {
             var cell = document.createElement('td');
             cell.setAttribute('height', 120);
             cell.setAttribute('width', 120);
             cell.setAttribute('align', 'center');
             cell.setAttribute('valign', 'center');
-			cell.classList.add('col' + j,'row' + i);
-			if (i == j) {
-				cell.classList.add('diagonal0');
-			}
+            cell.classList.add('col' + j,'row' + i);
+            if (i == j) {
+                cell.classList.add('diagonal0');
+            }
             if (j == N_SIZE - i - 1) {
-				cell.classList.add('diagonal1');
-			}
-			cell.identifier = identifier;
+                cell.classList.add('diagonal1');
+            }
+            cell.identifier = identifier;
             cell.addEventListener("click", set);
-			row.appendChild(cell)
-			boxes.push(cell);
-			identifier += identifier;
-		}
-	}
+            row.appendChild(cell)
+            boxes.push(cell);
+            identifier += identifier;
+        }
+    }
 
     document.getElementById("tictactoe").appendChild(board);
-		startNewGame();
+    startNewGame();
 }
 
 // New game
 function startNewGame() {
-	score = {
-		"X": 0,
-		"O": 0
-	};
-	moves = 0;
-	turn = "X";
-	boxes.forEach(function (square) {
-		square.innerHTML = EMPTY;
-	});
+    score = {
+        "X": 0,
+        "O": 0
+    };
+    moves = 0;
+    turn = "X";
+    boxes.forEach(function (square) {
+        square.innerHTML = EMPTY;
+    });
 }
 
 // Check if a win or not
 function win(clicked) {
-	// Get all cell classes
-	var memberOf = clicked.className.split(/\s+/);
-	for (var i = 0; i < memberOf.length; i++) {
-		var testClass = '.' + memberOf[i];
+    // Get all cell classes
+    var memberOf = clicked.className.split(/\s+/);
+    for (var i = 0; i < memberOf.length; i++) {
+        var testClass = '.' + memberOf[i];
         var items = contains('#tictactoe ' + testClass, turn);
-		// winning condition: turn == N_SIZE
-		if (items.length == N_SIZE) {
-			return true;
-		}
-	}
-	return false;
+        // winning condition: turn == N_SIZE
+        if (items.length == N_SIZE) {
+            return true;
+        }
+    }
+    return false;
 }
 
 function contains(selector, text) {
@@ -77,22 +77,22 @@ function contains(selector, text) {
 
 // Sets clicked square and also updates the turn.
 function set() {
-	if (this.innerHTML !== EMPTY) {
-		return;
-	}
-	this.innerHTML = turn;
-	moves += 1;
-	score[turn] += this.identifier;
-	if (win(this)) {
-		alert('Winner: Player ' + turn);
-		startNewGame();
-	} else if (moves === N_SIZE * N_SIZE) {
-		alert("Draw");
-		startNewGame();
-	} else {
-		turn = turn === "X" ? "O" : "X";
-		document.getElementById('turn').textContent = 'Player ' + turn;
-	}
+    if (this.innerHTML !== EMPTY) {
+        return;
+    }
+    this.innerHTML = turn;
+    moves += 1;
+    score[turn] += this.identifier;
+    if (win(this)) {
+        alert('Winner: Player ' + turn);
+        startNewGame();
+    } else if (moves === N_SIZE * N_SIZE) {
+        alert("Draw");
+        startNewGame();
+    } else {
+        turn = turn === "X" ? "O" : "X";
+        document.getElementById('turn').textContent = 'Player ' + turn;
+    }
 }
 
 init();
